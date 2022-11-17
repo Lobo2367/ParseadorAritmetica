da9a0ed36723a95111cafa59f8d3864a86d6165d
Lobo2367 <kingdd2367@gmail.com>
2022-11-17 18:29:53 -0400

    Agregar archivos de proyecto.

---
+++ ParseadorAritmetica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Antlr4.Runtime;
+using ParseadorAritmetica;
+
+namespace ParseadorAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string operacion = "2+6/3+2*13";
+
+            Console.WriteLine($"Input: {operacion} \n");
+            ICharStream charStream = CharStreams.fromString(operacion);
+            aritmeticaLexer lexer = new aritmeticaLexer(charStream);
+            CommonTokenStream token = new CommonTokenStream(lexer);
+            aritmeticaParser parser = new aritmeticaParser(token);
+            var tree = parser.aritmetica();
+            Aritmetica aritmetica = new Aritmetica();
+            int res = aritmetica.Visit(tree);
+            
+
+        }
+    }
+}
